2acd26c235deda3221e22ce56e851e3852ff7617
theabhayprajapati <theabhayprajapati@gmail.com>
2022-05-27 21:35:12 +0530

    add / router

--- main.js
+++ main.js
@@ -4,7 +4,14 @@ import leetcode from './api/leetcode.js';
 const app = express();
 app.use('/leetcode', leetcode);
 const PORT = process.env.PORT || 3000;
-
+app.get('/', (req, res) => {
+    res.json({
+        this: "this is leetcode api",
+        leetcode: "https://leetcode.com/abhayprajapati",
+        github: "https://github.com/theabhayprajapati",
+        twitter: "https://twitter.com/abhayprajapati_"
+    })
+})
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 })
